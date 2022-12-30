37fba3319564a91b3a06cbdd7c41f2956fd5de63
Daniel Lucas <danyyel770@gmail.com>
2022-12-30 11:09:06 +0100

    final working draft

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const colors = require('colors');
 const {router} = require('./routes/goalRoutes')
@@ -18,5 +19,19 @@ app.use(errorHandler)
 app.use(cors())
 app.use('/api/goals',router);
 app.use('/api/users/',userRouter);
+
+//Server frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
+
+
+    app.get('*',(req,res) => res.sendFile(path.
+    resolve(__dirname,'../','frontend','build','index.html')));
+}else{
+    app.get('/',(req,res) =>{
+        res.send('Please set to production')
+    })
+}
+
 
 app.listen(port,() => console.log(`Server listening on port:${port}`.cyan.underline))
